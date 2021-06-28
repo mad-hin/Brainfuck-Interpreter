0e88ae7b003fc503cc339602ac9fa17a8d4deb67
marco <marcotamcn@gmail.com>
2021-06-28 17:16:51 +0800

    Added arguments in the program

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This project is a simple brainfuck interpreter written in C++
 1. Type `cmake .` to generate the makefile\
 (Note: If your cmake does not meet the minimum required version, just change it from the `CMakeLists.txt`)
 2. Type `make` to compile it
-3. Type `./brainfuck <File with brainfuck code>` to run your brainfuck code
+3. Type `./brainfuck -i <File with brainfuck code>` to run your brainfuck code
 ## Help Request
 I am **not too familiar** with C++ and this is my first time to write a interpreter, so I may make some silly mistake.\
 If I make any mistake please point it out for me through the ```Issue```.\
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <string>
+#include <cstring>
 #include "interpreter.h"
 
 int main(int argc, char *argv[]) {
     if (argc >= 2) {
-        FILE *source = fopen(argv[1], "r");
-        if (source) {
-            command::getFile(source);
-        } else {
-            fprintf(stderr, "Could not open source file %s\n", argv[1]);
-            exit(1);
+        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+
+        } else if (strcmp(argv[1], "-i") == 0) {
+            // get to interpreter mode
+            FILE *source = fopen(argv[2], "r");
+            if (source) {
+                command::getFile(source);
+            } else {
+                fprintf(stderr, "Could not open source file %s\n", argv[2]);
+                exit(1);
+            }
         }
     }
     return 0;
